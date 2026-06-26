fb7dd5a5c7f8113af9b671846c93026e4e691042
rahaf ali <rahaf1423rahaf@gmail.com>
2026-06-26 15:44:12 +0300

    window

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("text to voice")
+
+icon = PhotoImage(file='logo.png')
+window.iconphoto(True, icon)
+
+window.config(background="#FDF4AF")
+
+window.mainloop()
