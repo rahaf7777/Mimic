6f72e586e186f7b86a3b2fe39a4c0ff160926e91
rahaf ali <rahaf1423rahaf@gmail.com>
2026-06-26 17:33:06 +0300

    add label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+# window
 window = Tk()
 window.geometry("420x420")
 window.title("text to voice")
@@ -8,5 +9,25 @@ icon = PhotoImage(file='logo.png')
 window.iconphoto(True, icon)
 
 window.config(background="#FDF4AF")
+
+# label
+
+photo = PhotoImage(file='pic2.png').subsample(2, 2)
+label = Label(window, text="put ur text",
+              font=('Arial', 40, 'bold'),
+              fg='#6FBEB2', bg="#FDF4AF",
+              image=photo, compound='bottom')
+label.pack()
+
+# buttons
+
+def click():
+    print("u clicked the button")
+
+button = Button(window,
+                text="convert",
+                command=click)
+button.pack()
+
 
 window.mainloop()
